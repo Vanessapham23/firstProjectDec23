eb56366b9537bf55f4d849e5fd4bc7413bacfae9
Vanessa Pham <vanessapham7931@gmail.com>
2024-06-13 22:49:19 -0500

    LearnGit2

---
+++ src/test/java/variousConcepts/LearnGit2Yall.java
@@ -0,0 +1,24 @@
+package variousConcepts;
+
+public class LearnGit2Yall {
+public static void main(String[] args) {
+	System.out.println("this is the git 2");
+}
+public void printMe1() { 
+	
+	
+	
+}
+public void printMe2() { 
+	
+	System.out.println("this is the git 2");
+	
+	
+}
+public void printMe3() { 
+	
+	System.out.println("this is the git 2");
+	
+	
+}
+}
